9786fc41ae4bac7d07b31976b4496b58707cc0e4
definitelyNotTheCluelessBoy <133683889+definitelyNotTheCluelessBoy@users.noreply.github.com>
2023-10-01 17:35:14 +0200

    commit

---
+++ src/test/java/database_project_API_prototype/databaseAPI/DatabaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package database_project_API_prototype.databaseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
